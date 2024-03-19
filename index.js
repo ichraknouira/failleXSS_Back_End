d85bf2b5134f179bef52c081a1a61504afea94a2
ichraknouira <inouira@mobelite.fr>
2024-03-19 13:24:45 +0100

    feat(xss): add cookie for authentification systel
    add cors options
    add cookie parser
    create and remove cookie
    add new route to check auth status
    
    feat/xss-002/add-cookie

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require("express");
 const cors = require("cors");
-
+const cookieParser = require("cookie-parser");
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(cookieParser());
 app.use("/login", (req, res) => {
+  res.cookie("token", "this a secret token", {
+    httpOnly: true,
+    maxAge: 360000,
+  });
   res.send({
-    token: "This is a secret token",
+    authenticated: true,
+    message: "Successful Authentication",
   });
 });
+app.use("/logout", (req, res) => {
+  res.cookie("token", null, {
+    httpOnly: true,
+    maxAge: 360000,
+  });
+  res.send({
+    authenticated: false,
+    message: "Successful logout",
+  });
+});
+app.use("/auth-status", (req, res) => {
+  if (req.cookies?.token === "this a secret token")
+    res.send({
+      isAuthenticated: true,
+    });
+  else
+    res.send({
+      isAuthenticated: false,
+    });
+});
+
 app.listen(8080, () => console.log(`API is active on http://localhost:8080`));
