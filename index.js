d65918784d8fd7f3109cdad799f1e6278f1d01e4
ichraknouira <inouira@mobelite.fr>
2024-03-21 13:30:21 +0100

    add comment

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const app = express();
-
+// add corsOptions and allow origin with credentails true
 const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true,
 };
+// add option cors and cookieParser
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use("/login", (req, res) => {
+  // create cookie with flag httpOnly
+
   res.cookie("token", "this a secret token", {
     httpOnly: true,
     maxAge: 360000,
@@ -20,6 +23,8 @@ app.use("/login", (req, res) => {
   });
 });
 app.use("/logout", (req, res) => {
+  // delete cookie with flag httpOnly
+
   res.cookie("token", null, {
     httpOnly: true,
     maxAge: 360000,
@@ -30,6 +35,8 @@ app.use("/logout", (req, res) => {
   });
 });
 app.use("/auth-status", (req, res) => {
+  // check if cookie token exist or not
+
   if (req.cookies?.token === "this a secret token")
     res.send({
       isAuthenticated: true,
